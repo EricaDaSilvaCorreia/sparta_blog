e40fd7128ea407af26b2a3d620af071346ac3722
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-10-03 14:01:56 +0100

    Blog MAAK 9: Comment Controller and routes added

--- config/routes.rb
+++ config/routes.rb
@@ -2,6 +2,9 @@ Rails.application.routes.draw do
   root 'static#landing'
 
   resources :users
-  resources :blogs
+  resources :blogs do
+    resources :comments
+  end
+
   # For details on the DSL available within this file, see http://guides.rubyonrails.org/routing.html
 end
