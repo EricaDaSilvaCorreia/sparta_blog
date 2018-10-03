924bda320067aeb78e72d7578e5d82655fb18e6b
Tech-A60 <tech-a60@Tech-A60s-MacBook-Air.local>
2018-10-03 15:25:56 +0100

    Blog MAAK 10: Added Full CRUD to nested Comments, and Model for comments and other minor changes

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,6 +1,12 @@
 class User < ApplicationRecord
   has_many :blogs
+  has_many :comments
 
   validates :first_name, :presence => true
   validates :last_name, :presence => true
+
+  def fullname
+    "#{first_name} #{last_name}"
+
+  end
 end
